64bda2b6f35aa08ed78f45f22fa46a4ce3fb0e09
David Eyk <deyk@indigoag.com>
2022-05-24 16:14:57 -0700

    Add personal settings

---
+++ personal/framesize.el
@@ -0,0 +1,3 @@
+(add-to-list 'default-frame-alist '(height . 52))
+(add-to-list 'default-frame-alist '(width . 160))
+(when window-system (set-frame-size (selected-frame) 160 52))
---
+++ personal/keybindings.el
@@ -0,0 +1,25 @@
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+
+(global-set-key (kbd "s-c") 'kill-ring-save) ; ⌘-c = Copy
+(global-set-key (kbd "s-x") 'kill-region) ; ⌘-x = Cut
+(global-set-key (kbd "s-v") 'yank) ; ⌘-v = Paste
+(global-set-key (kbd "s-a") 'mark-whole-buffer) ; ⌘-a = Select all
+(global-set-key (kbd "s-z") 'undo) ; ⌘-z = Undo
+
+(global-set-key (kbd "s-+") 'text-scale-increase)
+(global-set-key (kbd "s--") 'text-scale-decrease)
+
+
+(global-set-key (kbd "s-w") 'delete-frame)
+(global-set-key (kbd "s-n") 'make-frame)
+(global-set-key (kbd "s-`") 'other-frame)
+(global-set-key (kbd "s-z") 'undo-tree-undo)
+(global-set-key (kbd "s-s")
+                (lambda ()
+                  (interactive)
+                  (call-interactively (key-binding "\C-x\C-s"))))
+(global-set-key (kbd "s-q")
+                (lambda ()
+                  (interactive)
+                  (call-interactively (key-binding "\C-x\C-c"))))

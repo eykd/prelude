5f5c9b1297ac9c5a4e3b168d55d5ef03fb8956d7
David Eyk <deyk@indigoag.com>
2022-09-08 10:18:55 -0700

    Make sure MELPA is installed

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,2 +1,9 @@
-(eval-when-compile
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+ (eval-when-compile
   (require 'use-package))

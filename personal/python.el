7b9fded1740d01362187d5740db3181afc782576
David Eyk <deyk@indigoag.com>
2023-02-06 08:53:09 -0800

    Improve rainbow-mode in python

--- personal/python.el
+++ personal/python.el
@@ -1,1 +1,10 @@
+(custom-set-variables
+ '(rainbow-identifiers-faces-to-override
+   '(font-lock-variable-name-face
+     font-lock-function-name-face
+     font-lock-type-face
+     font-lock-builtin-face))
+
+ )
 (add-hook 'python-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'python-mode-hook 'rainbow-delimiters-mode)
